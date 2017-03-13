57f55fa4f068eac9ddfa041fe29ae9c56f0c84d2
G. Chalauri <g.chalauri@mia.gov.ge>
2017-03-13 20:42:02 +0400

    load data to components

--- app/src/main/java/ge/gchalauri/weatherapp/weatherapi/MainActivity.java
+++ app/src/main/java/ge/gchalauri/weatherapp/weatherapi/MainActivity.java
@@ -1,8 +1,15 @@
 package ge.gchalauri.weatherapp.weatherapi;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -13,6 +20,7 @@ import ge.gchalauri.weatherapp.weatherapi.networking.ApixuService;
 import ge.gchalauri.weatherapp.weatherapi.utils.Consts;
 import io.realm.Realm;
 import io.realm.RealmResults;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -21,21 +29,44 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText citySearch;
+    ImageView icon;
+    TextView temperature, cityData, note;
+
+    ApixuService service;
+
+    LinearLayout dataLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void search(View view) throws IOException {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Consts.URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        ApixuService service = retrofit.create(ApixuService.class);
-        System.out.println("BASE " + Consts.URL);
-        Call<ApixuResponse> callService = service.getWeather(Consts.DEFAULT_CITY, Consts.API_KEY);
+        service = retrofit.create(ApixuService.class);
+
+        citySearch = (EditText) findViewById(R.id.city);
+        icon = (ImageView) findViewById(R.id.icon_data);
+        temperature = (TextView) findViewById(R.id.temperature);
+        cityData = (TextView) findViewById(R.id.city_data);
+        note = (TextView) findViewById(R.id.note);
+
+        dataLayout = (LinearLayout)findViewById(R.id.data_layout);
+    }
+
+    public void search(View view) throws IOException {
+
+        String city = citySearch.getText().toString();
+
+        if (city == null || city.trim().equals("")) {
+            city = Consts.DEFAULT_CITY;
+        }
+
+        Call<ApixuResponse> callService = service.getWeather(city, Consts.API_KEY);
 
         System.out.println("URL " + callService.request().url());
         callService.enqueue(new Callback<ApixuResponse>() {
@@ -43,20 +74,20 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<ApixuResponse> call, Response<ApixuResponse> response) {
                 System.out.println(response.body());
                 ApixuResponse ar = response.body();
-                System.out.println(ar.getLocation().getLat());
 
-                save(ar);
-
+                System.out.println(ar.getCurrent().getCondition().getIcon());
+                loadData(ar);
             }
 
             @Override
             public void onFailure(Call<ApixuResponse> call, Throwable t) {
                 System.out.println("ERR" + t.getMessage());
+                Toast.makeText(MainActivity.this, "ERROR DURING LOADING DATA", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    public void save(ApixuResponse response) {
+  /*  public void save(ApixuResponse response) {
         Realm.init(getApplicationContext());
         Realm realm = Realm.getDefaultInstance();
 
@@ -75,14 +106,51 @@ public class MainActivity extends AppCompatActivity {
         realm.commitTransaction();
 
         System.out.println(realm.getPath());
-    }
+    }*/
 
-    public void find(View view) throws IOException {
+  /*  public void find(View view) throws IOException {
         //TODO test realm retrieve data
         Realm.init(getApplicationContext());
         Realm realm = Realm.getDefaultInstance();
 
         final RealmResults<ApixuResponse> puppies = realm.where(ApixuResponse.class).findAll();
         System.out.println("SIZE  " + puppies.size());
+    }*/
+
+    private void loadImage(String url) {
+        Call<ResponseBody> call = service.fetchCaptcha(url);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        // display the image data in a ImageView or save it
+                        Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                        icon.setImageBitmap(bitmap);
+                    } else {
+                        icon.setImageBitmap(null);
+                    }
+                } else {
+                    icon.setImageBitmap(null);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                icon.setImageBitmap(null);
+                Toast.makeText(MainActivity.this, "ERROR DURING LOADING ICON", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadData(ApixuResponse response){
+        cityData.setText(response.getLocation().getName().toUpperCase());
+        temperature.setText(response.getCurrent().getTemp_c()+ " ℃");
+        note.setText(response.getCurrent().getCondition().getText());
+
+        dataLayout.setVisibility(View.VISIBLE);
+
+        String iconUrl = response.getCurrent().getCondition().getIcon();
+        loadImage(iconUrl);
     }
 }
--- app/src/main/java/ge/gchalauri/weatherapp/weatherapi/networking/ApixuService.java
+++ app/src/main/java/ge/gchalauri/weatherapp/weatherapi/networking/ApixuService.java
@@ -2,9 +2,11 @@ package ge.gchalauri.weatherapp.weatherapi.networking;
 
 import ge.gchalauri.weatherapp.weatherapi.entities.ApixuResponse;
 import ge.gchalauri.weatherapp.weatherapi.utils.Consts;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import retrofit2.http.Url;
 
 /**
  * Created by G.Chalauri on 03/07/17.
@@ -14,5 +16,8 @@ public interface ApixuService {
 
     @GET("current.json")
     Call<ApixuResponse> getWeather(@Query("q") String city,@Query("key") String key);
+
+    @GET
+    Call<ResponseBody> fetchCaptcha(@Url String url);
 
 }
